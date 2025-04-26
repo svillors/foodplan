b8943c9fc7c9802d0c33bce6de998f4fc27b34a3
Roman <grachev.ro@gmail.com>
2025-04-26 18:38:49 +0300

    Update project #8

--- order/urls.py
+++ order/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('order/', create_order, name='create_order'),
-    path('payment/<int:order_id>/', payment, name='payment'),
+    # path('payment/<int:order_id>/', payment, name='payment'),
     path('lk/', lk_view, name='lk'),
-    path('payment/details/<int:order_id>/', payment_details, name='payment_details'),
+    path('payment/', payment, name='payment'),
+    path('payment_details/', payment_details, name='payment_details'),    
 ]
