3f2c1744ab280b68164f8970db79d9807a339f71
Roman <grachev.ro@gmail.com>
2025-04-26 15:25:54 +0300

    Adding index recipe link #foodapp/urls.py

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from foodapp import views
+from recipes.views import recipe_details
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recipes/<int:pk>/', recipe_details, name='recipe_detail')
 ]
