1ae7ed6b689c0b859153f539fd769ec2c88b2a43
Roman <grachev.ro@gmail.com>
2025-04-26 15:23:20 +0300

    Adding index recipe link

--- foodapp/views.py
+++ foodapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 
 def index(request):
-    return render(request, "index.html")
+    carousel_recipes = Recipe.objects.order_by('-id')[:5]
+    return render(request, "index.html", {'carousel_recipes': carousel_recipes})
