c051aa240afd5c4297aa83c64e9574c2e3425aed
svillors <tntzator9@gmail.com>
2025-04-28 18:26:46 +0500

    Delete allegry from admin

--- order/admin.py
+++ order/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 
-from .models import Order, Allergy
+from .models import Order
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['user', 'menu_type', 'duration', 'is_paid']
     list_filter = ('is_paid',)
-    
-
-
-
-@admin.register(Allergy)
-class AllergyAdmin(admin.ModelAdmin):
-    pass
-
